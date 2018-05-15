ca6ee447a1ac1fc7cfa2d0df8bb65b0a540af963
chinmaypkelkar <chinmaypkelkar@gmail.com>
2018-05-14 23:10:10 -0500

    first commit

---
+++ SamuraiApp.Domain/Quote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.Domain
+{
+    public class Quote
+    {
+        public int id { get; set; }
+        public string Test { get; set; }
+        public Samurai Samurai { get; set; }
+        public int SamuraiId { get;set }
+    }
+
+}
---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c12742c9-1ace-4d07-9729-806530dcc543")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
